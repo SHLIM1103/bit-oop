895cb3e5b00d5ed934adb096e7668ec1b45e1326
SHLIM1103 <73277983+SHLIM1103@users.noreply.github.com>
2020-11-16 19:47:58 +0900

    19:47

--- day04/Calculation.java
+++ day04/Calculation.java
@@ -1,5 +1,22 @@
 package day04;
 
-public class Calculation {
-
+class Calculation {
+	int number1 ;
+	int number2 ;
+	
+	void setNumber1(int number1) {
+		this.number1 = number1;
+	}
+	
+	int getNumber1() {
+		return number1 ;
+	}
+	
+	void setNumber2(int number2){
+		this.number2 = number2;
+	}
+	
+	int getNumber2() {
+		return number2;
+	}
 }
--- day04/Main.java
+++ day04/Main.java
@@ -1,5 +1,42 @@
 package day04;
 
-public class Main {
+import java.util.Scanner;
+import day04.Calculation;
 
+class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Calculation cal = new Calculation();
+
+		System.out.print("number1: ");
+		int number1 = scanner.nextInt();
+		cal.setNumber1(number1);
+
+		System.out.print("number2: ");
+		int number2 = scanner.nextInt();
+		cal.setNumber2(number2);
+
+		System.out.println("number1 + number2 = " + add(cal.getNumber1(), cal.getNumber2()));
+		System.out.println("number1 - number2 = " + subtract(cal.getNumber1(), cal.getNumber2()));
+		System.out.println("number1 * number2 = " + multiply(cal.getNumber1(), cal.getNumber2()));
+		System.out.println("number1 / number2 = " + divide(cal.getNumber1(), cal.getNumber2()));
+
+		scanner.close();
+	}
+
+	static int add(int number1, int number2) {
+		return number1 + number2;
+	}
+
+	static int subtract(int number1, int number2) {
+		return number1 - number2;
+	}
+
+	static int multiply(int number1, int number2) {
+		return number1 * number2;
+	}
+
+	static double divide(int number1, int number2) {
+		return number1 / (double) number2;
+	}
 }
